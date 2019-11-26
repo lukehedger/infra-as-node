b388d29d0a68ba5821c2284faf116f5834d43b35
lukehedger <lh4@pm.me>
2019-11-26 11:55:54 +0000

    Remove ignored generated files

--- infrastructure/lib/infrastructure-stack.d.ts
+++
@@ -1,6 +0,0 @@
-import { CfnParametersCode } from "@aws-cdk/aws-lambda";
-import { Construct, Stack, StackProps } from "@aws-cdk/core";
-export declare class InfrastructureStack extends Stack {
-    readonly lambdaCode: CfnParametersCode;
-    constructor(scope: Construct, id: string, props?: StackProps);
-}
--- pipeline/lib/pipeline-stack.d.ts
+++
@@ -1,8 +0,0 @@
-import { Construct, Stack, StackProps } from "@aws-cdk/core";
-import { CfnParametersCode } from "@aws-cdk/aws-lambda";
-export interface PipelineStackProps extends StackProps {
-    readonly lambdaCode: CfnParametersCode;
-}
-export declare class PipelineStack extends Stack {
-    constructor(scope: Construct, id: string, props?: PipelineStackProps);
-}
