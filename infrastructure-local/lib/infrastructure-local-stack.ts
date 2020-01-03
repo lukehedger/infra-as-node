42954153f3dca06d31fc7f56f2f73b51ae670195
lukehedger <lh4@pm.me>
2020-01-03 11:02:30 +0000

    Port infra to local infra

--- infrastructure-local/lib/infrastructure-local-stack.ts
+++ infrastructure-local/lib/infrastructure-local-stack.ts
@@ -1,24 +1,80 @@
 import { LambdaRestApi } from "@aws-cdk/aws-apigateway";
-import { Code, Function, Runtime } from "@aws-cdk/aws-lambda";
-import { Construct, Stack, StackProps } from "@aws-cdk/core";
+import { Stream, StreamEncryption } from "@aws-cdk/aws-kinesis";
+import {
+  Code,
+  Function,
+  Runtime,
+  StartingPosition,
+  Tracing
+} from "@aws-cdk/aws-lambda";
+import {
+  KinesisEventSource,
+  SqsEventSource
+} from "@aws-cdk/aws-lambda-event-sources";
+import { Queue, QueueEncryption } from "@aws-cdk/aws-sqs";
+import { Construct, Duration, Stack, StackProps } from "@aws-cdk/core";
 
 export class InfrastructureStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const ping = new Function(this, "PingHandler", {
-      code: Code.fromAsset("../ping/lib"),
-      handler: "ping.handler",
-      runtime: Runtime.NODEJS_10_X
+    const kinesisStreamName = "KinesisStream";
+
+    const kinesisStream = new Stream(this, kinesisStreamName, {
+      encryption: StreamEncryption.KMS
     });
 
-    const api = new LambdaRestApi(this, "PingEndpoint", {
-      handler: ping,
+    const kinesisConsumerDLQ = new Queue(this, "KinesisConsumerDLQ", {
+      encryption: QueueEncryption.KMS_MANAGED,
+      visibilityTimeout: Duration.seconds(30),
+      receiveMessageWaitTime: Duration.seconds(20)
+    });
+
+    const dlqConsumerLambda = new Function(this, "DLQConsumerHandler", {
+      code: Code.fromAsset("../dlq-consumer/lib"),
+      handler: "consumer.handler",
+      runtime: Runtime.NODEJS_10_X,
+      tracing: Tracing.ACTIVE
+    });
+
+    dlqConsumerLambda.addEventSource(
+      new SqsEventSource(kinesisConsumerDLQ, {
+        batchSize: 10
+      })
+    );
+
+    const kinesisConsumerLambda = new Function(this, "KinesisConsumerHandler", {
+      code: Code.fromAsset("../kinesis-consumer/lib"),
+      deadLetterQueue: kinesisConsumerDLQ,
+      handler: "consumer.handler",
+      runtime: Runtime.NODEJS_10_X,
+      tracing: Tracing.ACTIVE
+    });
+
+    kinesisConsumerLambda.addEventSource(
+      new KinesisEventSource(kinesisStream, {
+        batchSize: 100,
+        startingPosition: StartingPosition.TRIM_HORIZON
+      })
+    );
+
+    const kinesisProducerLambda = new Function(this, "KinesisProducerHandler", {
+      code: Code.fromAsset("../kinesis-producer/lib"),
+      environment: {
+        KINESIS_STREAM_NAME: kinesisStreamName
+      },
+      handler: "producer.handler",
+      runtime: Runtime.NODEJS_10_X,
+      tracing: Tracing.ACTIVE
+    });
+
+    const api = new LambdaRestApi(this, "KinesisProducerEndpoint", {
+      handler: kinesisProducerLambda,
       proxy: false
     });
 
-    const pingResource = api.root.addResource("ping");
+    const kinesisProducerResource = api.root.addResource("kinesis-producer");
 
-    pingResource.addMethod("POST");
+    kinesisProducerResource.addMethod("POST");
   }
 }
