a18746f73a312a19c25ee3473fb03de577533ec4
lukehedger <lh4@pm.me>
2020-03-05 09:36:18 +0000

    Consistent API name

--- cloud-infrastructure/lib/infrastructure-stack.ts
+++ cloud-infrastructure/lib/infrastructure-stack.ts
@@ -55,15 +55,11 @@ export class InfrastructureStack extends Stack {
       }
     );
 
-    const apiName = process.env.GITHUB_PR_NUMBER
-      ? `EventBridgeProducerEndpoint-${process.env.GITHUB_PR_NUMBER}`
-      : "EventBridgeProducerEndpoint";
-
     const stageName = process.env.GITHUB_PR_NUMBER
       ? `integration-${process.env.GITHUB_PR_NUMBER}`
       : "prod";
 
-    const api = new LambdaRestApi(this, apiName, {
+    const api = new LambdaRestApi(this, "EventBridgeProducerEndpoint", {
       handler: eventbridgeProducerLambda,
       proxy: false,
       deployOptions: {
