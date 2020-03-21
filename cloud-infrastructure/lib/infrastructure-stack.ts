b7f4b70b0802799dd851eb16bb64406541b3b6b4
lukehedger <lh4@pm.me>
2020-03-21 19:26:41 +0000

    Add dashboard and alarms

--- cloud-infrastructure/lib/infrastructure-stack.ts
+++ cloud-infrastructure/lib/infrastructure-stack.ts
@@ -1,5 +1,7 @@
 import { LambdaRestApi } from "@aws-cdk/aws-apigateway";
 import { CloudFrontWebDistribution } from "@aws-cdk/aws-cloudfront";
+import { Dashboard, GraphWidget, Row } from "@aws-cdk/aws-cloudwatch";
+import { SnsAction } from "@aws-cdk/aws-cloudwatch-actions";
 import { EventBus, Rule } from "@aws-cdk/aws-events";
 import { LambdaFunction } from "@aws-cdk/aws-events-targets";
 import {
@@ -9,9 +11,13 @@ import {
   Runtime,
   Tracing
 } from "@aws-cdk/aws-lambda";
-import { SnsDestination } from "@aws-cdk/aws-lambda-destinations";
+import {
+  SnsDestination,
+  SqsDestination
+} from "@aws-cdk/aws-lambda-destinations";
 import { Bucket } from "@aws-cdk/aws-s3";
 import { Topic } from "@aws-cdk/aws-sns";
+import { Queue, QueueEncryption } from "@aws-cdk/aws-sqs";
 import { Construct, RemovalPolicy, Stack, StackProps } from "@aws-cdk/core";
 
 export class InfrastructureStack extends Stack {
@@ -26,11 +32,6 @@ export class InfrastructureStack extends Stack {
     this.eventbridgeProducerLambdaCode = Code.cfnParameters();
     this.eventbridgeS3LambdaCode = Code.cfnParameters();
 
-    const eventBridgeConsumerSuccessTopic = new Topic(
-      this,
-      "EventBridgeConsumerSuccessTopic"
-    );
-
     const eventbridgeConsumerRule = new Rule(this, "EventBridgeConsumerRule", {
       eventPattern: {
         detail: {
@@ -39,6 +40,19 @@ export class InfrastructureStack extends Stack {
         source: ["com.ian"]
       }
     });
+
+    const eventBridgeConsumerSuccessTopic = new Topic(
+      this,
+      "EventBridgeConsumerSuccessTopic"
+    );
+
+    const eventBridgeConsumerFailureQueue = new Queue(
+      this,
+      "EventBridgeConsumerFailureQueue",
+      {
+        encryption: QueueEncryption.KMS_MANAGED
+      }
+    );
 
     const eventbridgeConsumerLambda = new Function(
       this,
@@ -49,6 +63,7 @@ export class InfrastructureStack extends Stack {
           ? `EventBridgeConsumer-Integration-${process.env.GITHUB_PR_NUMBER}`
           : "EventBridgeConsumer-Production",
         handler: "consumer.handler",
+        onFailure: new SqsDestination(eventBridgeConsumerFailureQueue),
         onSuccess: new SnsDestination(eventBridgeConsumerSuccessTopic),
         runtime: Runtime.NODEJS_12_X,
         tracing: Tracing.ACTIVE
@@ -67,6 +82,10 @@ export class InfrastructureStack extends Stack {
       bucketName: eventLogBucketName.toLowerCase()
     });
 
+    const eventLogFailureQueue = new Queue(this, "EventLogFailureQueue", {
+      encryption: QueueEncryption.KMS_MANAGED
+    });
+
     const eventbridgeS3Lambda = new Function(this, "EventBridgeS3Handler", {
       code: this.eventbridgeS3LambdaCode,
       environment: {
@@ -76,6 +95,7 @@ export class InfrastructureStack extends Stack {
         ? `EventBridgeS3-Integration-${process.env.GITHUB_PR_NUMBER}`
         : "EventBridgeS3-Production",
       handler: "consumer.handler",
+      onFailure: new SqsDestination(eventLogFailureQueue),
       runtime: Runtime.NODEJS_12_X,
       tracing: Tracing.ACTIVE
     });
@@ -132,6 +152,7 @@ export class InfrastructureStack extends Stack {
       bucketName: staticAppBucketName,
       publicReadAccess: true,
       removalPolicy: RemovalPolicy.DESTROY,
+      websiteErrorDocument: "index.html",
       websiteIndexDocument: "index.html"
     });
 
@@ -143,5 +164,140 @@ export class InfrastructureStack extends Stack {
         }
       ]
     });
+
+    const godModeDashboardName = process.env.GITHUB_PR_NUMBER
+      ? `Dashboard-Integration-${process.env.GITHUB_PR_NUMBER}`
+      : "Dashboard-Production";
+
+    const godModeDashboard = new Dashboard(this, "GodModeDashboard", {
+      dashboardName: godModeDashboardName
+    });
+
+    godModeDashboard.addWidgets(
+      new Row(
+        new GraphWidget({
+          left: [
+            eventbridgeConsumerLambda.metricInvocations(),
+            eventbridgeProducerLambda.metricInvocations(),
+            eventbridgeS3Lambda.metricInvocations()
+          ],
+          title: "Lambda Invocations"
+        }),
+        new GraphWidget({
+          left: [
+            eventbridgeConsumerLambda.metricDuration(),
+            eventbridgeProducerLambda.metricDuration(),
+            eventbridgeS3Lambda.metricDuration()
+          ],
+          title: "Lambda Duration"
+        }),
+        new GraphWidget({
+          left: [
+            eventbridgeConsumerLambda.metricErrors(),
+            eventbridgeProducerLambda.metricErrors(),
+            eventbridgeS3Lambda.metricErrors()
+          ],
+          title: "Lambda Errors"
+        }),
+        new GraphWidget({
+          left: [
+            eventbridgeConsumerLambda.metricThrottles(),
+            eventbridgeProducerLambda.metricThrottles(),
+            eventbridgeS3Lambda.metricThrottles()
+          ],
+          title: "Lambda Throttles"
+        })
+      )
+    );
+
+    const applicationAlertsTopic = new Topic(this, "ApplicationAlertsTopic", {
+      topicName: process.env.GITHUB_PR_NUMBER
+        ? `ApplicationAlertsTopic-Integration-${process.env.GITHUB_PR_NUMBER}`
+        : "ApplicationAlertsTopic-Production"
+    });
+
+    const eventbridgeConsumerLambdaErrorsAlarm = eventbridgeConsumerLambda
+      .metricErrors()
+      .createAlarm(this, "EventbridgeConsumerLambdaErrorsAlarm", {
+        alarmName: process.env.GITHUB_PR_NUMBER
+          ? `EventbridgeConsumerLambdaErrorsAlarm-Integration-${process.env.GITHUB_PR_NUMBER}`
+          : "EventbridgeConsumerLambdaErrorsAlarm-Production",
+        evaluationPeriods: 2,
+        threshold: 10
+      });
+
+    eventbridgeConsumerLambdaErrorsAlarm.addAlarmAction(
+      new SnsAction(applicationAlertsTopic)
+    );
+
+    const eventbridgeConsumerLambdaThrottlesAlarm = eventbridgeConsumerLambda
+      .metricThrottles()
+      .createAlarm(this, "EventbridgeConsumerLambdaThrottlesAlarm", {
+        alarmName: process.env.GITHUB_PR_NUMBER
+          ? `EventbridgeConsumerLambdaThrottlesAlarm-Integration-${process.env.GITHUB_PR_NUMBER}`
+          : "EventbridgeConsumerLambdaThrottlesAlarm-Production",
+        evaluationPeriods: 2,
+        threshold: 10
+      });
+
+    eventbridgeConsumerLambdaThrottlesAlarm.addAlarmAction(
+      new SnsAction(applicationAlertsTopic)
+    );
+
+    const eventbridgeProducerLambdaErrorsAlarm = eventbridgeProducerLambda
+      .metricErrors()
+      .createAlarm(this, "EventbridgeProducerLambdaErrorsAlarm", {
+        alarmName: process.env.GITHUB_PR_NUMBER
+          ? `EventbridgeProducerLambdaErrorsAlarm-Integration-${process.env.GITHUB_PR_NUMBER}`
+          : "EventbridgeProducerLambdaErrorsAlarm-Production",
+        evaluationPeriods: 2,
+        threshold: 10
+      });
+
+    eventbridgeProducerLambdaErrorsAlarm.addAlarmAction(
+      new SnsAction(applicationAlertsTopic)
+    );
+
+    const eventbridgeProducerLambdaThrottlesAlarm = eventbridgeProducerLambda
+      .metricThrottles()
+      .createAlarm(this, "EventbridgeProducerLambdaThrottlesAlarm", {
+        alarmName: process.env.GITHUB_PR_NUMBER
+          ? `EventbridgeProducerLambdaThrottlesAlarm-Integration-${process.env.GITHUB_PR_NUMBER}`
+          : "EventbridgeProducerLambdaThrottlesAlarm-Production",
+        evaluationPeriods: 2,
+        threshold: 10
+      });
+
+    eventbridgeProducerLambdaThrottlesAlarm.addAlarmAction(
+      new SnsAction(applicationAlertsTopic)
+    );
+
+    const eventbridgeS3LambdaErrorsAlarm = eventbridgeS3Lambda
+      .metricErrors()
+      .createAlarm(this, "EventbridgeS3LambdaErrorsAlarm", {
+        alarmName: process.env.GITHUB_PR_NUMBER
+          ? `EventbridgeS3LambdaErrorsAlarm-Integration-${process.env.GITHUB_PR_NUMBER}`
+          : "EventbridgeS3LambdaErrorsAlarm-Production",
+        evaluationPeriods: 2,
+        threshold: 10
+      });
+
+    eventbridgeS3LambdaErrorsAlarm.addAlarmAction(
+      new SnsAction(applicationAlertsTopic)
+    );
+
+    const eventbridgeS3LambdaThrottlesAlarm = eventbridgeS3Lambda
+      .metricThrottles()
+      .createAlarm(this, "EventbridgeS3LambdaThrottlesAlarm", {
+        alarmName: process.env.GITHUB_PR_NUMBER
+          ? `EventbridgeS3LambdaThrottlesAlarm-Integration-${process.env.GITHUB_PR_NUMBER}`
+          : "EventbridgeS3LambdaThrottlesAlarm-Production",
+        evaluationPeriods: 2,
+        threshold: 10
+      });
+
+    eventbridgeS3LambdaThrottlesAlarm.addAlarmAction(
+      new SnsAction(applicationAlertsTopic)
+    );
   }
 }
