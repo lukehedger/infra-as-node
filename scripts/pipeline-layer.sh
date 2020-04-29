4620bfd7fad519033760ecb35c8181929a2e463d
lukehedger <lh4@pm.me>
2020-04-29 11:46:07 +0100

    Remove pipeline layer symlinks

--- scripts/pipeline-layer.sh
+++ scripts/pipeline-layer.sh
@@ -5,3 +5,6 @@ npm install --no-package-lock --prefix pipeline-status --production
 
 # Copy node_modules to Lambda Layer directory
 cp -r pipeline-status/node_modules/ pipeline-layer/nodejs/node_modules
+
+# Remove symlinks from Lambda Layer node_modules
+rm -rf pipeline-layer/nodejs/node_modules/.bin
